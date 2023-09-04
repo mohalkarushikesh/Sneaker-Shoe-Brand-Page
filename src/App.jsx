78752979b34b512ba7e0cc53d73620036787f251
Rushikesh Mohalkar <82998291+mohalkarushikesh@users.noreply.github.com>
2023-09-04 13:07:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
